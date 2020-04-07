b54abc71dc6667f67ce36e122610b2eba0000068
mymissingcontinues@163.com <mymissingcontinues@163.com>
2020-04-07 19:14:03 +0800

    初次提交blog-web

---
+++ config/config.js
@@ -0,0 +1,17 @@
+function getHost(){
+    return "http://127.0.0.1:8806";
+}
+
+function getServerAddress(){
+//	return "http://localhost:8080/aserver";
+//	return "http://10.1.7.153:8806";
+    return getHost() + "/app-server";
+}
+
+function getImageBaseUrl(){
+    return "http://182.92.169.183:80/download";
+}
+
+function appVersion(){
+    return "视联汇后台系统V1.0.1"
+}
