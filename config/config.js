94c42326cd822c84b9bd4db794d27fb66a3813d5
Xinhuix <mymissingcontinues@163.com>
2020-04-07 23:06:37 +0800

    修改config getHost地址

--- config/config.js
+++ config/config.js
@@ -1,10 +1,8 @@
 function getHost(){
-    return "http://127.0.0.1:8806";
+    return "http://182.92.169.183:8806";
 }
 
 function getServerAddress(){
-//	return "http://localhost:8080/aserver";
-//	return "http://10.1.7.153:8806";
     return getHost() + "/app-server";
 }
 
