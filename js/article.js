525967b6123970701b30e813a887394865f5c947
Xinhuix <mymissingcontinues@163.com>
2020-04-10 00:47:23 +0800

    添加模糊查询功能

--- js/article.js
+++ js/article.js
@@ -1,7 +1,7 @@
 document.write("<script src='https://unpkg.com/axios/dist/axios.min.js'></script>");
 
 
-layui.use('form', function(){
+/*layui.use('form', function(){
     var form = layui.form;
     form.on('submit(searchForm)', function(data){
         var keywords=$("#keywords").val();
@@ -10,27 +10,12 @@ layui.use('form', function(){
             layer.msg('请输入要搜索的关键字');
             return false;
         }
-        axios({
-            method: 'get',
-            url: getHost() +'/rest/getInquireArticle?title='+keywords,
-            responseType: 'json',
-            data: {
 
-            }
-        })
-            .then( (response)=> {
-                console.log(response);
-                console.log(response.data.data);
-                this.dataAll = response.data.data;
-            })
-            .catch((error)=> {
-                console.log(error);
-            });
         search();
         //getData ====> return data
         return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
     });
-});
+});*/
 
 
 
@@ -54,5 +39,7 @@ function search() {
 }
 
 function classifyList(id) {
+
+    console.log("hahaha")
 	layer.msg('功能要自己写哦！');
 }
